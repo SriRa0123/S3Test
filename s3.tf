0b1af1ef966f53ff610b95cca6ba975508da165f
SriRa0123 <sadhumuralidhar9@gmail.com>
2025-04-04 20:30:57 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" 
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "my-simple-s3-bucket"
+  bucket = "my-simple-s3-bucket-123214323432523213124234452343242134"
   acl    = "private"
   
   tags = {
